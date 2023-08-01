a0c6425c86d46777b7a9847973413f822bfedba1
Diwash Karki <diwaskarki96@gmail.com>
2023-08-01 18:13:26 +0545

    learned about basic javascript

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("Hello");
+
+//
+//ES5
+var person ="ram";
+
+person="shyam";
+
+console.log(person);
+//ES6
+const person1 = "ram";
+console.log({ person1});
+//Let
+let person2 = "Ram";
+person2 = "shyam";
+console.log(person2);
