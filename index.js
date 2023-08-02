2d04dc19182fb0dd25557c7599aa573e85e38920
Diwash Karki <diwaskarki96@gmail.com>
2023-08-02 17:00:41 +0545

    learned interaction

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-console.log("Hello");
+// console.log("Hello");
 
-//
-//ES5
-var person ="ram";
+// //
+// //ES5
+// var person ="ram";
 
-person="shyam";
+// person="shyam";
 
-console.log(person);
-//ES6
-const person1 = "ram";
-console.log({ person1});
-//Let
-let person2 = "Ram";
-person2 = "shyam";
-console.log(person2);
+// console.log(person);
+// //ES6
+// const person1 = "ram";
+// console.log({ person1});
+// //Let
+// let person2 = "Ram";
+// person2 = "shyam";
+// console.log(person2);
+//chapter 2 "Interaction"
+//alert("message");
+//prompt("enter your name");
+//confirm("are you sure");
+const name = prompt("Enter your name");
+const password = prompt("Enter your password");
+
+
+if (name ==="diwas" && password ==="123") {
+    confirm("Are you sure") && 
+    alert("Welcome to our Website");
+}
