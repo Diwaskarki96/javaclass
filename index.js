6ce4fdac69727356d072a0297b62380a82c99d20
Diwash Karki <diwaskarki96@gmail.com>
2023-08-03 17:08:43 +0545

    learned function

--- index.js
+++ index.js
@@ -64,11 +64,73 @@
 // }
 
 //do while loop
-let a = 1;
+// let a = 1;
+// do {
+//   console.log(a);
+//   a++;
+// } while (a <= 5);
 
-do {
-  console.log(a);
-  a++;
-} while (a <= 5);
+//switch case
+// let day = 3;
+// let dayName;
+
+// switch (day) {
+//   case 1:
+//     dayName = "Sunday";
+//     break;
+//   case 2:
+//     dayName = "Monday";
+//     break;
+//   case 3:
+//     dayName = "Tuesday";
+//     break;
+//   case 4:
+//     dayName = "Wednesday";
+//     break;
+//   case 5:
+//     dayName = "Thursday";
+//     break;
+//   case 6:
+//     dayName = "Friday";
+//     break;
+//   case 7:
+//     dayName = "Saturday";
+//     break;
+//   default:
+//     dayName = "Invalid day";
+// }
+
+// console.log("Today is " + dayName);
+
+//function 
+//ES5
+// const hello(){
+//   console.log("hello");
+// };
+//ES6
+//const hello = () =>{
+//   console.log("hello");
+// };
+//function that says hello
+// function sayHello() {
+//   console.log("Hello!");
+// }
+// sayHello();
+//function that says hello to users
+// function sayHello() {
+//   console.log("Hello!")
+//   console.log("Hello to user!");
+// }
+// sayHello();
+// const hello = (a,b)=>{
+//   console.log(a+b)
+// }
+// hello(a,b);
+//function adding 2 number
+const sum = (a,b)=>{
+  let c = a+b
+    console.log(c)
+  }
+  sum(5,6);
 
 
