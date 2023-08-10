e0260f49b45528721bbb57466024b71c189487d3
Diwash Karki <diwaskarki96@gmail.com>
2023-08-10 17:43:11 +0545

    added comment in qrcode

--- node.js/qrcode.js
+++ node.js/qrcode.js
@@ -1,12 +1,24 @@
 const QrCode = require('qrcode');
 const http = require('http');
-http.createServer(async(req,res) =>{
-    const url = "https://github.com/Diwaskarki96";
-    const qrData = await QrCode.toDataURL(url); //data-image.src...........
-    const imgTag = "<img src ='"+qrData+"'/>";
 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-   res.write(imgTag);
-   res.end();
+http.createServer(async (req, res) => {
+  const url = "https://github.com/Diwaskarki96";
+  
+  // Generate the data URL for the QR code
+  const qrData = await QrCode.toDataURL(url);
+
+  // Create an HTML <img> tag with the QR code data URL
+  const imgTag = "<img src='" + qrData + "'/>";
+
+  // Set the response's content type to 'text/html'
+  res.writeHead(200, {'Content-Type': 'text/html'});
+
+  // Write the <img> tag to the response
+  res.write(imgTag);
+
+  // End the response
+  res.end();
 }).listen(8000);
-console.log("surver is running in 8000 port");
+
+// Log a message to the console indicating the server is running
+console.log("Server is running on port 8000");
