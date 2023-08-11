a2d62df406bd7e85ef284a9e99d6a36d99b76d6e
Diwash Karki <diwaskarki96@gmail.com>
2023-08-11 18:06:48 +0545

    learned convert currency

--- node.js/convertcurrency.js
+++ node.js/convertcurrency.js
@@ -1,12 +1,45 @@
-const CC = require('currency-converter-lt')
-let currencyConverter = new CC({from:"USD", to:"JPY", amount:100})
+
+// const CC = require('currency-converter-lt')
+// let currencyConverter = new CC({from:"USD", to:"JPY", amount:100})
 
 
-const converter = () => {
-   const convo =  currencyConverter.convert().then((response) => {
-        console.log(response) //or do something else
-    })
-    return convo;
+// const converter = () => {
+//    const convo =  currencyConverter.convert().then((response) => {
+//         console.log(response) //or do something else
+//     })
+//     return convo;
+// }
+// console.log(converter());
+
+// sir ko
+const CC = require('currency-converter-lt');
+const currencyConverter = new CC();
+
+const converter = async (from, to, amount) => {
+  const result = await currencyConverter
+    .from(from)
+    .to(to)
+    .amount(amount)
+    .convert();
+  return result;
+};
+
+async function solve() {
+  try {
+    const answer = await converter("INR", "NPR", 100);
+    console.log({ answer });
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
 }
-console.log(converter());
+
+solve();
+
+
+
+
+
+
+
+
 
