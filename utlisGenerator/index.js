0ea9ff7efafd72591b6f6ee8f1c683b0fc75759c
Diwash Karki <diwaskarki96@gmail.com>
2023-08-15 16:59:00 +0545

    added comment

--- utlisGenerator/index.js
+++ utlisGenerator/index.js
@@ -1,9 +1,11 @@
-const express = require('express')
-const app = express()
-
-app.get('/', function (req, res) {
-  res.send('Hello World from diwash karki')
-})
-
-app.listen(3000);
-console.log("App running on port 3000");
+//Import the Express.js library and create an instance of the Express application:
+// const express = require("express");
+// const app = express();
+//Define a route handler for the root URL ("/"). When a user accesses this URL using an HTTP GET request, the server will execute the provided callback function:
+// app.get("/", function (req, res) {
+//   res.send("Hello World from diwash karki");
+// });
+//Start the Express server and make it listen on port 3000 for incoming requests:
+// app.listen(3000);
+// Print a message to the console indicating that the server is running on port 3000:
+// console.log("App running on port 3000");
